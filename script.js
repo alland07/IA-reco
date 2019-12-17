c9fa5dc0939f3682212e7df1616a7354f8b4585f
AllanD <55087762+alland07@users.noreply.github.com>
2019-12-17 13:16:10 +0100

    fonctionnel

--- script.js
+++ script.js
@@ -5,30 +5,28 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 
 var grammar = '#JSGF V1.0;'
 
-    var recognition = new SpeechRecognition();
-    var speechRecognitionList = new SpeechGrammarList();
-    speechRecognitionList.addFromString(grammar, 1);
-    recognition.grammars = speechRecognitionList;
-    recognition.lang = 'fr';
-    recognition.interimResults = false;
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.lang = 'fr';
+recognition.interimResults = false;
+
 
 recognition.onresult = function(event) {
-    var last = event.results.length - 1;
-    var command = event.results[last][0].transcript;
-    message.textContent = 'Voice Input: ' + command + '.';
-    if(command.toLowerCase() === 'Choisi Allan'){
-        document.querySelector('#chkAllan').checked = true;
-    }
-    else if (command.toLowerCase() === 'Choisi Eidan'){
-        document.querySelector('#chkEidan').checked = true;
-    }
-        else if (command.toLowerCase() === 'Choisi Elise'){
-        document.querySelector('#chkElise').checked = true;
-    }
-    else if (command.toLowerCase() === 'Choisi Noa'){
-        document.querySelector('#chkNoa').checked = true;
-    }   
-};
+var last = event.results.length - 1;
+var command = event.results[last][0].transcript;
+message.textContent = 'Voice Input: ' + command + '.';
+if(command == 'choisis Alan'){
+    document.querySelector('#chkAllan').checked = true;
+} else if (command == 'choisis Eden'){
+    document.querySelector('#chkEidan').checked = true;
+} else if (command == 'choisis Élise'){
+    document.querySelector('#chkElise').checked = true;
+} else if (command == 'choisis Noah'){
+    document.querySelector('#chkNoa').checked = true;
+}   
+}
 
 recognition.onspeechend = function() {
     recognition.stop();

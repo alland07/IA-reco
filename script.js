cf106a73b7cc30a5ca4aea307afc26f9582b2b99
AllanD <55087762+alland07@users.noreply.github.com>
2020-01-06 15:45:51 +0100

    Quelques changements

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var message = document.querySelector('#message');
+let message = document.querySelector('#message');
+let reponse = document.querySelector('#réponse');
 
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+let SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+let SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 
-var grammar = '#JSGF V1.0;'
+let grammar = '#JSGF V1.0;'
 
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
+let recognition = new SpeechRecognition();
+let speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
 recognition.lang = 'fr';
@@ -14,9 +15,11 @@ recognition.interimResults = false;
 
 
 recognition.onresult = function(event) {
-var last = event.results.length - 1;
-var command = event.results[last][0].transcript;
-message.textContent = 'Voice Input: ' + command + '.';
+
+let last = event.results.length - 1;
+let command = event.results[last][0].transcript;
+message.textContent = 'Vous avez dit: ' + command + '.';
+
 if(command == 'choisis Alan'){
     document.querySelector('#chkAllan').checked = true;
 } else if (command == 'choisis Eden'){
@@ -25,7 +28,18 @@ if(command == 'choisis Alan'){
     document.querySelector('#chkElise').checked = true;
 } else if (command == 'choisis Noah'){
     document.querySelector('#chkNoa').checked = true;
-}   
+}
+ else if (command == 'sélectionne Alan'){
+    document.querySelector('#chkAllan').checked = false;
+} else if (command == 'sélectionne Eden'){
+    document.querySelector('#chkEidan').checked = false;
+} else if (command == 'sélectionne Élise'){
+    document.querySelector('#chkElise').checked = false;
+} else if (command == 'sélectionne Noah'){
+    document.querySelector('#chkNoa').checked = false;
+} 
+
+  
 }
 
 recognition.onspeechend = function() {
@@ -33,7 +47,7 @@ recognition.onspeechend = function() {
 };
 
 recognition.onerror = function(event) {
-    message.textContent = 'Error occurred in recognition: ' + event.error;
+    message.textContent = 'La reconnaissance n\'a pas fonctionnée ' + event.error;
 }        
 
 document.querySelector('#btnGiveCommand').addEventListener('click', function(){
